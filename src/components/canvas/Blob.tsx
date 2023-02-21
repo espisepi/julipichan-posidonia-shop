c7ce72ea94c6e8c9446806977f04702c095fe49c
espisepi <josdomesp@gmail.com>
2023-02-21 19:46:37 +0100

    added model with draco compression (show in blob component)

--- src/components/canvas/Blob.tsx
+++ src/components/canvas/Blob.tsx
@@ -1,12 +1,32 @@
+import * as THREE from 'three'
 import { useState } from 'react'
 import { useRouter } from 'next/router'
-import { useCursor, MeshDistortMaterial } from '@react-three/drei'
+import { useCursor, MeshDistortMaterial, useGLTF } from '@react-three/drei'
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader'
+
+type GLTFResult = GLTF & {
+  nodes: {
+    texturedMesh: THREE.Mesh
+  }
+  materials: {
+    ['TextureAtlas_1001']: THREE.MeshPhysicalMaterial
+  }
+}
+
+
 
 export default function Blob({ route, ...props }) {
   const router = useRouter()
   const [hovered, hover] = useState(false)
   useCursor(hovered)
+
+  const { nodes, materials } = useGLTF('models/test/untitledDraco.gltf', true) as unknown as GLTFResult
+  console.log(nodes)
+  console.log(materials)
+//return <mesh material={materials['Material.001']} geometry={(nodes.Suzanne as Mesh).geometry} />
+
   return (
+    <>
     <mesh
       onClick={() => router.push(route)}
       onPointerOver={() => hover(true)}
@@ -15,5 +35,7 @@ export default function Blob({ route, ...props }) {
       <sphereGeometry args={[1, 64, 64]} />
       <MeshDistortMaterial roughness={0} color={hovered ? 'hotpink' : '#1fb2f5'} />
     </mesh>
+    <mesh material={materials['TextureAtlas_1001']} geometry={(nodes.texturedMesh as THREE.Mesh).geometry} />
+    </>
   )
 }
