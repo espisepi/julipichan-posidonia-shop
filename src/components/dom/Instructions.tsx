fb25fe6affc2807010ac329c238551350f1876ed
espisepi <josdomesp@gmail.com>
2023-02-22 18:34:55 +0100

    remove dom instructions

--- src/components/dom/Instructions.tsx
+++ src/components/dom/Instructions.tsx
@@ -2,9 +2,9 @@ export default function Instructions({ children }) {
   return (
     <div
       className='absolute max-w-lg px-10 py-8 text-sm bg-zinc-800 rounded-lg shadow-xl md:text-base top-16 left-1/2 transform -translate-x-1/2'
-      style={{ maxWidth: 'calc(100% - 28px)' }}>
+      style={{ maxWidth: 'calc(100% - 28px)', zIndex:5 }}>
       <p className='hidden mb-8 md:block'>{children}</p>
-      <div className='tracking-wider'>
+      {/* <div className='tracking-wider'>
         Update your header in <span className='text-green-200'> @/config </span>
         <br />
         The layout is set in <span className='text-green-200'>@/pages/_app</span>
@@ -19,7 +19,7 @@ export default function Instructions({ children }) {
         <br />
         Delete <span className='text-red-200'> @/components/canvas/Blob</span> &{' '}
         <span className='text-red-200'>/Logo</span>
-      </div>
+      </div> */}
     </div>
   )
 }
