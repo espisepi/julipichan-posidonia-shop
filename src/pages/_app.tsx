9afaa954c3ddf5235a84b7d6b66d1aa2b0df6c08
espisepi <josdomesp@gmail.com>
2023-02-27 14:40:58 +0100

    added missing providers in _app page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,10 @@ import Header from '@/config'
 import Layout from '@/components/dom/Layout'
 import '@/styles/index.css'
 
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import { SWRConfig } from 'swr';
+
+
 import { SessionProvider } from 'next-auth/react'
 
 
@@ -13,6 +17,7 @@ import { lightTheme } from '../material-ui/themes';
 
 import { CartProvider } from '@/features/cart'
 import { UiProvider } from '@/features/ui'
+import { AuthProvider } from '@/features/auth'
 
 const Scene = dynamic(() => import('@/components/canvas/Scene'), { ssr: true })
 
@@ -21,30 +26,37 @@ export default function App({ Component, pageProps = { title: 'index' } }) {
   return (
     <>
     <SessionProvider>
-      <Header title={pageProps.title} />
-      <Layout ref={ref}>
+      <PayPalScriptProvider options={{ 'client-id': process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '' }}>
+        <SWRConfig 
+            value={{
+              fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+            }}
+          >
+          <Header title={pageProps.title} />
+          <Layout ref={ref}>
+            <AuthProvider>
+              <CartProvider>
+                <UiProvider>
+                  <ThemeProvider theme={ lightTheme}>
+                    <CssBaseline />
+                    <Component {...pageProps} />
+                  </ThemeProvider>
 
-        <CartProvider>
-          <UiProvider>
+                  {/* The canvas can either be in front of the dom or behind. If it is in front it can overlay contents.
+                  * Setting the event source to a shared parent allows both the dom and the canvas to receive events.
+                  * Since the event source is now shared, the canvas would block events, we prevent that with pointerEvents: none. */}
+                  {Component?.canvas && (
+                    <Scene className='pointer-events-none' eventSource={ref} eventPrefix='client'>
+                      {Component.canvas(pageProps)}
+                    </Scene>
+                  )}
 
-          <ThemeProvider theme={ lightTheme}>
-            <CssBaseline />
-            <Component {...pageProps} />
-          </ThemeProvider>
-
-          {/* The canvas can either be in front of the dom or behind. If it is in front it can overlay contents.
-          * Setting the event source to a shared parent allows both the dom and the canvas to receive events.
-          * Since the event source is now shared, the canvas would block events, we prevent that with pointerEvents: none. */}
-          {Component?.canvas && (
-            <Scene className='pointer-events-none' eventSource={ref} eventPrefix='client'>
-              {Component.canvas(pageProps)}
-            </Scene>
-          )}
-
-          </UiProvider>
-        </CartProvider>
-
-      </Layout>
+                </UiProvider>
+              </CartProvider>
+            </AuthProvider>
+          </Layout>
+        </SWRConfig>
+      </PayPalScriptProvider>
     </SessionProvider>
     </>
   )
