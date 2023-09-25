2dc2d5d4981514507a6601f2eb32fd203ec21a45
espisepi <sepinaco@icloud.com>
2023-09-25 22:32:13 +0200

    navbar with tags like hoodie,shirts and hat

--- src/features/next-teslo/ui/components/dom/navbar/Navbar.tsx
+++ src/features/next-teslo/ui/components/dom/navbar/Navbar.tsx
@@ -1,16 +1,15 @@
-import { useContext, useState } from 'react';
-import NextLink from 'next/link';
-import { useRouter } from 'next/router';
+import { useContext, useState } from 'react'
+import NextLink from 'next/link'
+import { useRouter } from 'next/router'
 
-import { AppBar, Badge, Box, Button, IconButton, Input, InputAdornment, Link, Toolbar, Typography } from '@mui/material';
-import { ClearOutlined, SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material';
+import { AppBar, Badge, Box, Button, IconButton, Input, InputAdornment, Link, Toolbar, Typography } from '@mui/material'
+import { ClearOutlined, SearchOutlined, ShoppingCartOutlined } from '@mui/icons-material'
 
 //import { CartContext, UiContext } from '../../../context';
-import { CartContext } from '@/features/next-teslo';
-import { UiContext } from '@/features/next-teslo';
+import { CartContext } from '@/features/next-teslo'
+import { UiContext } from '@/features/next-teslo'
 
-import styles from './Navbar.module.css';
-
+import styles from './Navbar.module.css'
 
 /**
  * 
@@ -33,35 +32,31 @@ import styles from './Navbar.module.css';
  * 
  */
 
-
 export const Navbar = () => {
+  const { asPath, push } = useRouter()
+  const { toggleSideMenu } = useContext(UiContext)
+  const { numberOfItems } = useContext(CartContext)
 
-    const { asPath, push } = useRouter();
-    const { toggleSideMenu } = useContext( UiContext );
-    const { numberOfItems } = useContext( CartContext );
+  const [searchTerm, setSearchTerm] = useState('')
+  const [isSearchVisible, setIsSearchVisible] = useState(false)
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const onSearchTerm = () => {
+    if (searchTerm.trim().length === 0) return
+    push(`/search/${searchTerm}`)
+  }
 
-    const onSearchTerm = () => {
-        if( searchTerm.trim().length === 0 ) return;
-        push(`/search/${ searchTerm }`);
-    }
+  return (
+    <AppBar>
+      <div className={styles['navbar-bg-color']}>
+        <Toolbar>
+          <Link href='/' component={NextLink} display='flex' alignItems='center'>
+            <Typography variant='h6'>Teslo |</Typography>
+            <Typography sx={{ ml: 0.5 }}>Shop</Typography>
+          </Link>
 
-    
+          <Box flex={1} />
 
-    return (
-        <AppBar>
-            <div className={ styles['navbar-bg-color'] }>
-            <Toolbar>
-                    <Link href="/" component={NextLink} display='flex' alignItems='center'>
-                        <Typography variant='h6'>Teslo |</Typography>
-                        <Typography sx={{ ml: 0.5 }}>Shop</Typography>
-                    </Link>  
-
-                <Box flex={ 1 } />
-
-                <Box sx={{ display: isSearchVisible ? 'none' : { xs: 'none', sm: 'block' } }}
+          {/* <Box sx={{ display: isSearchVisible ? 'none' : { xs: 'none', sm: 'block' } }}
                     className="fadeIn">
                         <Link href='/category/men'  component={NextLink}>
                             <Button color={ asPath === '/category/men' ? 'primary':'info'}>Hombres</Button>
@@ -72,59 +67,56 @@ export const Navbar = () => {
                         <Link href='/category/kid' component={NextLink}>
                             <Button color={ asPath === '/category/kid' ? 'primary':'info'}>Niños</Button>
                         </Link>
-                </Box>
+                </Box> */}
 
+          <Box sx={{ display: isSearchVisible ? 'none' : { xs: 'none', sm: 'block' } }} className='fadeIn'>
+            <Link href='/tags/hoodie' component={NextLink}>
+              <Button color={asPath === '/tags/hoodie' ? 'primary' : 'info'}>Hoodie</Button>
+            </Link>
+            <Link href='/tags/shirt' component={NextLink}>
+              <Button color={asPath === '/tags/shirt' ? 'primary' : 'info'}>Shirt</Button>
+            </Link>
+            <Link href='/tags/hats' component={NextLink}>
+              <Button color={asPath === '/tags/hats' ? 'primary' : 'info'}>Hats</Button>
+            </Link>
+          </Box>
 
-                <Box flex={ 1 } />
-                
-                
+          <Box flex={1} />
 
-                {/* Pantallas pantallas grandes */}
-                {
-                    isSearchVisible 
-                        ? (
-                            <Input
-                                sx={{ display: { xs: 'none', sm: 'flex' } }}
-                                className='fadeIn'
-                                autoFocus
-                                value={ searchTerm }
-                                onChange={ (e) => setSearchTerm( e.target.value ) }
-                                onKeyPress={ (e) => e.key === 'Enter' ? onSearchTerm() : null }
-                                type='text'
-                                placeholder="Buscar..."
-                                endAdornment={
-                                    <InputAdornment position="end">
-                                        <IconButton
-                                            onClick={ () => setIsSearchVisible(false) }
-                                        >
-                                            <ClearOutlined />
-                                        </IconButton>
-                                    </InputAdornment>
-                                }
-                            />
-                        )
-                    : 
-                    (
-                        <IconButton 
-                            onClick={ () => setIsSearchVisible(true) }
-                            className="fadeIn"
-                            sx={{ display: { xs: 'none', sm: 'flex' } }}
-                        >
-                            <SearchOutlined />
-                        </IconButton>
-                    )
-                }
+          {/* Pantallas pantallas grandes */}
+          {isSearchVisible ? (
+            <Input
+              sx={{ display: { xs: 'none', sm: 'flex' } }}
+              className='fadeIn'
+              autoFocus
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              onKeyPress={(e) => (e.key === 'Enter' ? onSearchTerm() : null)}
+              type='text'
+              placeholder='Buscar...'
+              endAdornment={
+                <InputAdornment position='end'>
+                  <IconButton onClick={() => setIsSearchVisible(false)}>
+                    <ClearOutlined />
+                  </IconButton>
+                </InputAdornment>
+              }
+            />
+          ) : (
+            <IconButton
+              onClick={() => setIsSearchVisible(true)}
+              className='fadeIn'
+              sx={{ display: { xs: 'none', sm: 'flex' } }}>
+              <SearchOutlined />
+            </IconButton>
+          )}
 
+          {/* Pantallas pequeñas */}
+          <IconButton sx={{ display: { xs: 'flex', sm: 'none' } }} onClick={toggleSideMenu}>
+            <SearchOutlined />
+          </IconButton>
 
-                {/* Pantallas pequeñas */}
-                <IconButton
-                    sx={{ display: { xs: 'flex', sm: 'none' } }}
-                    onClick={ toggleSideMenu }
-                >
-                    <SearchOutlined />
-                </IconButton>
-
-                {/* <NextLink href="/cart" passHref>
+          {/* <NextLink href="/cart" passHref>
                     <Link>
                         <IconButton>
                             <Badge badgeContent={ numberOfItems > 9 ? '+9': numberOfItems  } color="secondary">
@@ -134,13 +126,9 @@ export const Navbar = () => {
                     </Link>
                 </NextLink> */}
 
-
-                <Button onClick={ toggleSideMenu }>
-                    Menú
-                </Button>
-
-            </Toolbar>
-            </div>
-        </AppBar>
-    )
+          <Button onClick={toggleSideMenu}>Menú</Button>
+        </Toolbar>
+      </div>
+    </AppBar>
+  )
 }
